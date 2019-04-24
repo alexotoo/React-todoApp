e47734252a139ed76258ebe09f0ab29f30f11ebf
abraham otoo <abrahamotoo@abrahams-MacBook-Pro.local>
2019-04-24 17:07:02 +0200

    setting the method to get values from the todo inputs

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import TodoInput from "./Component/TodoInput";
 import uuid from "uuid";
 
 export default class App extends Component {
+  state = {
+    items: [],
+    id: 0,
+    item: "",
+    editItem: false
+  };
+  // to avoid binding in contructor us arrow functions
+  handleChange = c => {
+    this.setState({
+      item: c.target.value
+    });
+  };
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 max-auto col-md-8 mt-4">
-            <h3 className="text-capitalize text-center">Todo Inputttt</h3>
-            <TodoInput />
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+            />
             <TodoList />
           </div>
         </div>
--- src/Component/TodoItem.js
+++ src/Component/TodoItem.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 export default class TodoItem extends Component {
   render() {
-    return <div>Hello this is TodoItem inside TodoList</div>;
+    return (
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+        <h6>title</h6>
+        <div className="pen-del-incons">
+          <span className="mx-2 text-success">
+            <i className="fas fa-pen" />
+          </span>
+          <span className="max-2 text-danger">
+            <i className="fas fa-trash" />
+          </span>
+        </div>
+      </li>
+    );
   }
 }
